21f44f39c4b41754058ef2d4236cd5adcd599e9d
mohammadhadi <mohammadHadi@smartec.ir>
2022-02-19 14:16:00 +0330

    Add project files.

---
+++ DiInjector.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ServiceCollectionAccessorService
+{
+    public static class DiInjector
+    {
+        public static IServiceCollection AddServiceCollectionAccessor(this IServiceCollection services)
+        {
+            services.AddScoped(n => new ServiceCollectionAccessor(services));
+            services.BuildServiceProvider().GetRequiredService<ServiceCollectionAccessor>();
+            return services;
+        }
+
+    }
+}
---
+++ ServiceCollectionAccessor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceCollectionAccessorService
+{
+    public class ServiceCollectionAccessor
+    {
+        public static IServiceCollection Services { get; private set; }
+
+        public ServiceCollectionAccessor(IServiceCollection services)
+        {
+            Services = services;
+        }
+    }
+}
